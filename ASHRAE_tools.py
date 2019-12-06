c9ac6fbe6fa858fc4af3beef5aa2451edcca6b52
Bruno Gonzalez <brunocgf@gmail.com>
2019-12-06 11:56:23 -0600

    primer modelo LR

--- ASHRAE_tools.py
+++ ASHRAE_tools.py
@@ -1,5 +1,12 @@
-## The meter id code
+import numpy as np
 
 def meter_dict():
     md = {0: 'electricity', 1: 'chilledwater', 2: 'steam', 3: 'hotwater'}
     return md
+
+def submit(row,y):
+    x = row
+    x['meter_reading'] = np.maximum(0,y)
+    x.set_index('row_id', inplace = True)
+    x.sort_values(inplace = True)
+    x.to_csv('./submission.csv')
