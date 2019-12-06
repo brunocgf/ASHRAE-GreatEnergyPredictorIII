9482b1c8ea4740c5ac46cbd10b7089a0df7826c8
Bruno Gonzalez <brunocgf@gmail.com>
2019-12-05 18:26:39 -0600

    modelo regresion

--- ASHRAE_data_prep.py
+++ ASHRAE_data_prep.py
@@ -1,9 +1,10 @@
 import numpy as np
 import pandas as pd
+from sklearn.impute import KNNImputer
 
 # Load data
 
-def all_data():
+def train_all():
 
     train = pd.read_csv('./data/train.csv')
     building_metadata = pd.read_csv('./data/building_metadata.csv')
@@ -31,13 +32,14 @@ def all_data():
 #Eliminar variables
 #train.drop('floor_count', inplace=True, axis=1)
 
-def num_data():
+def train_reg():
 
     train = pd.read_csv('./data/train.csv')
     building_metadata = pd.read_csv('./data/building_metadata.csv')
     weather_train = pd.read_csv('./data/weather_train.csv')
 
-    # Meter 0 correction
+    # Sort data for future imputation
+    train.sort_values(by=['building_id','timestamp'], inplace=True)
 
     # Merging data
     train = (train
@@ -47,13 +49,58 @@ def num_data():
     #Add dates variables
     train['timestamp'] = pd.to_datetime(train['timestamp'])
     train['hour'] = train.timestamp.dt.hour
-    train['day'] = train.timestamp.dt.normalize()
     train['wday'] = train.timestamp.dt.dayofweek
     train['week'] = train.timestamp.dt.weekofyear
+
+    #Eliminate problematic variables
     train.drop('timestamp', inplace=True, axis = 1)
     train.drop('primary_use', inplace=True, axis = 1)
+    train.drop('year_built', inplace=True, axis = 1)
+    train.drop('floor_count', inplace=True, axis = 1)
+    train.drop('precip_depth_1_hr', inplace=True, axis = 1)
+    train.drop('cloud_coverage', inplace=True, axis = 1)
+
+    # Imputation
+    train = train.interpolate()
 
     X_train = train.loc[:,train.columns != 'meter_reading']
     y_train = train.loc[:,train.columns == 'meter_reading']
 
     return X_train, y_train
+
+
+def test_reg():
+
+    test = pd.read_csv('./data/test.csv')
+    building_metadata = pd.read_csv('./data/building_metadata.csv')
+    weather_test = pd.read_csv('./data/weather_test.csv')
+
+    # Sort data for future imputation
+    test.sort_values(by=['building_id','timestamp'], inplace=True)
+
+    # Merging data
+    test = (test
+    .merge(building_metadata, on = 'building_id', how='left')
+    .merge(weather_test, on = ['site_id','timestamp'], how='left'))
+
+    #Add dates variables
+    test['timestamp'] = pd.to_datetime(test['timestamp'])
+    test['hour'] = test.timestamp.dt.hour
+    test['wday'] = test.timestamp.dt.dayofweek
+    test['week'] = test.timestamp.dt.weekofyear
+
+    #Eliminate problematic variables
+    test.drop('timestamp', inplace=True, axis = 1)
+    test.drop('primary_use', inplace=True, axis = 1)
+    test.drop('year_built', inplace=True, axis = 1)
+    test.drop('floor_count', inplace=True, axis = 1)
+    test.drop('precip_depth_1_hr', inplace=True, axis = 1)
+    test.drop('cloud_coverage', inplace=True, axis = 1)
+
+    # Imputation
+    test = test.interpolate()
+
+    X_test = test.loc[:,test.columns != 'row_id']
+    row = test.loc[:,test.columns == 'row_id']
+
+    return row, X_test
